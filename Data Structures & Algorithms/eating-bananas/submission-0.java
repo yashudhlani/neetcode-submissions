feac9e7906b4e420baccfa936a543958bf581958
Yash Udhlani <udhlaniyash@gmail.com>
2026-06-01 15:03:24 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = 0;
+        for (int pile: piles) {
+            max = Integer.max(max, pile);
+        }
+        int l = 1;
+        int r = max;
+        System.out.println("Max = " + max);
+
+        while (l < r) {
+            int mid = (l + r) >> 1;
+
+            int hours = 0;
+            for (int pile: piles) {
+                hours += mid >= pile ? 1 : pile % mid == 0 ? pile / mid: (pile/mid) + 1;
+            }
+            System.out.println(mid + " - " + hours);
+            if (hours <= h) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        } 
+
+        return l;
+
+    }
+}
