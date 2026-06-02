5064ac05fd30cc3758e0464a714895c520b5fd66
Yash Udhlani <udhlaniyash@gmail.com>
2026-06-02 15:29:03 +0530

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numDecodings(String s) {
+        // 3134626
+        int[] dp = new int[s.length()];
+        dp[0] = s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 1; i < s.length(); i++) {
+            dp[i] = s.charAt(i) == '0' ? 0 : dp[i - 1];
+            if (s.charAt(i - 1) == '1' || (s.charAt(i - 1) == '2' && s.charAt(i) < '7')) {
+                dp[i] = dp[i] + (i > 1 ? dp[i - 2] : 1);
+            }
+        }
+        return dp[s.length() - 1];
+    }
+    // 3 1 3 4 6 2 6
+    // 3 13 4 6 2 6
+    // 3 1 3 4 6 26
+    // 3 13 4 6 26
+}
